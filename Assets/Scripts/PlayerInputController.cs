5c2e3ecf8526eb9499ebe7b9697dcdc374ffb3f1
Ismail4537 <horispararoh457@gmail.com>
2025-11-08 17:15:28 +0700

    player

---
+++ Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputController : MonoBehaviour
+
+{
+    public Vector2 dir = Vector2.zero;
+    private Player player;
+
+    private void Awake()
+    {
+        player = GetComponent<Player>();
+    }
+
+    public void MoveInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            // Debug.Log("Move started");
+        }
+        if (context.performed)
+        {
+            // Debug.Log("Move performed");
+        }
+        if (context.canceled)
+        {
+            // Debug.Log("Move canceled");
+        }
+        dir = context.ReadValue<Vector2>();
+        // Debug.Log(dir);
+    }
+
+    public void JumpInput(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            // Debug.Log("Jump started");
+        }
+        if (context.performed)
+        {
+            if (player != null)
+            {
+                player.Jump();
+            }
+            // Debug.Log("Jump performed");
+        }
+        if (context.canceled)
+        {
+            // Debug.Log("Jump canceled");
+        }
+    }
+}
